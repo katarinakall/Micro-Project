4ad7524f3a01f7a35dfe539bc657528306bb2f62
Mathias Vessberg <mathias.vessberg@gmail.com>
2018-06-21 09:57:04 +0200

    Set border for player

---
+++ src/Border.java
@@ -0,0 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Border {
+    private List<Enemy> enemies = new ArrayList<Enemy>();
+}
--- src/MonsterGame.java
+++ src/MonsterGame.java
@@ -4,9 +4,11 @@ import com.googlecode.lanterna.terminal.Terminal;
 import com.googlecode.lanterna.terminal.TerminalSize;
 
 import java.nio.charset.Charset;
+import java.util.ArrayList;
 
 public class MonsterGame {
     public static void main(String[] args) throws InterruptedException {
+
         Terminal terminal = TerminalFacade.createTerminal(System.in,
                 System.out, Charset.forName("UTF8"));
         terminal.enterPrivateMode();
@@ -15,9 +17,22 @@ public class MonsterGame {
         terminal.moveCursor(hero.x, hero.y);
         terminal.putCharacter('\u263A');
 
-        Enemy alien = new Enemy(1,1);
+        Enemy alien = new Enemy(11,16);
         terminal.moveCursor(alien.x, alien.y);
-        terminal.putCharacter('\u2639');
+        terminal.putCharacter('\u26C4');
+
+        Enemy ghost = new Enemy(17,25);
+        terminal.moveCursor(ghost.x, ghost.y);
+        terminal.putCharacter('\u26C4');
+
+        Enemy monster = new Enemy(3,29);
+        terminal.moveCursor(monster.x, monster.y);
+        terminal.putCharacter('\u26C4');
+
+        Enemy janne = new Enemy(35,27);
+        terminal.moveCursor(janne.x, janne.y);
+        terminal.putCharacter('\u26C4');
+
 
         while (true) {
 //Wait for a key to be pressed
@@ -32,15 +47,23 @@ public class MonsterGame {
             switch (key.getKind()) {
                 case ArrowDown:
                     hero.y = hero.y +1;
+                    if (hero.y > 29)
+                        hero.y = 29;
                     break;
                 case ArrowUp:
                     hero.y = hero.y -1;
+                    if (hero.y < 0)
+                        hero.y = 0;
                     break;
                 case ArrowLeft:
                     hero.x = hero.x -1;
+                    if (hero.x < 0)
+                    hero.x = 0;
                     break;
                 case ArrowRight:
                     hero.x = hero.x +1;
+                    if (hero.x > 98)
+                        hero.x = 98;
                     break;
 
             }
@@ -50,6 +73,20 @@ public class MonsterGame {
 
             terminal.moveCursor(hero.x, hero.y);
             terminal.putCharacter('\u263A');
+
+            terminal.moveCursor(alien.x, alien.y);
+            terminal.putCharacter('\u26C4');
+
+            terminal.moveCursor(ghost.x, ghost.y);
+            terminal.putCharacter('\u26C4');
+
+            terminal.moveCursor(monster.x, monster.y);
+            terminal.putCharacter('\u26C4');
+
+            terminal.moveCursor(janne.x, janne.y);
+            terminal.putCharacter('\u26C4');
+
+
 
 
         }
